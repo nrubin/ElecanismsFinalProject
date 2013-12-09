4b1315ea549c4481ce4376ec41ce15fa67ef4f89
Noam <noamr@outlook.com>
2013-12-09 14:28:39 -0500

    added stepper driver files, new gitignore

---
+++ stepperdriver/USBComm.py
@@ -0,0 +1,69 @@
+import usb.core
+import time
+class USBComm:
+
+    def __init__(self):
+        print "Your UART cable better be plugged into the USB 2.0 port, or else kittens will cry"
+        self.SET_VELOCITY = 0
+        self.GET_VALS = 1
+        self.CLEAR_REV = 2
+        self.dev = usb.core.find(idVendor = 0x6666, idProduct = 0x0003)
+        if self.dev is None:
+            raise ValueError('no USB device found matching idVendor = 0x6666 and idProduct = 0x0003')
+        self.dev.set_configuration()
+
+    def close(self):
+        self.dev = None
+
+    def set_velocity(self, duty, direction):
+        try:
+            self.dev.ctrl_transfer(0x40, self.SET_VELOCITY, int(duty), int(direction))
+        except usb.core.USBError as e:
+            print e
+            print "Could not send SET_VELOCITY vendor request."
+            raise e
+
+    def clear_rev(self):
+        try:
+            self.dev.ctrl_transfer(0x40, self.CLEAR_REV)
+        except usb.core.USBError as e:
+            print e
+            print "Could not send CLEAR_REV vendor request."
+            raise e
+
+    def get_vals(self):
+        try:
+            ret = self.dev.ctrl_transfer(0xC0, self.GET_VALS, 0, 0, 8)
+        except usb.core.USBError:
+            print "Could not send GET_VALS vendor request."
+        else:
+            return [int(ret[0])+int(ret[1])*256, int(ret[2])+int(ret[3])*256, int(ret[4])+int(ret[5])*256, int(ret[6])+int(ret[7])*256]
+
+    def values(self):
+        l = self.get_vals()
+        rev = l[0]
+        fb = l[1]
+        dir_sense = l[2]
+        vemf = l[3]
+        print "rev = %s \n fb = %s \n dir = %s \n per = %s" % (rev,fb,dir_sense,vemf)
+        return {"rev":rev,"fb":fb,"dir_sense":dir_sense,"vemf":vemf}
+
+if __name__ == '__main__':
+    u = USBComm()
+    stack = [0]
+    # for speed in xrange(8000,50000,100):
+    #     u.set_vals(speed,0)
+    #     time.sleep(0.1)
+    #     current = u.get_vals()[1]
+    #     print "The speed is %s and the current detected is %s" % (speed,current)
+    speed = int(raw_input("speed: 0 - 65536 \n"))
+    direction = int(raw_input("direction: 1 or 0 \n"))
+    u.set_velocity(speed,direction)
+    while True:
+        u.values()
+        time.sleep(0.5)
+    # while True:
+    #     v = u.get_vals()[1]
+    #     if v > stack[0]:
+    #         stack = [v] + stack
+    #         print v
--- stepperdriver/stepperdriver.c
+++ stepperdriver/stepperdriver.c
@@ -13,12 +13,15 @@
 #define GET_VALS    1   // Vendor request that returns 2 unsigned integer values
 #define CLEAR_REV 2 //Vender request that clears the rev counter
 
-#define UARTTx &D[0]
-#define UARTRx &D[1]
-#define HeartBeatTimer &timer1
+#define HB_TIMER &timer3
+#define CLK_TIMER &timer2
+#define STARTUP_TIMER &timer1
+#define ENABLE &D[0]
+#define CLK &D[1]
+#define RESET &D[2] 
+#define CW &D[3]
 
-uint16_t val1, val2, received_char;
-
+uint16_t val1, val2, startup_counter,clk_out;
 
 
 void VendorRequests(void) {
@@ -26,6 +29,7 @@ void VendorRequests(void) {
 
     switch (USB_setup.bRequest) {
         case SET_VELOCITY:
+            val1 = USB_setup.wValue.w;
             BD[EP0IN].bytecount = 0;    // set EP0 IN byte count to 0 
             BD[EP0IN].status = 0xC8;    // send packet as DATA1, set UOWN bit
             break;
@@ -64,11 +68,6 @@ void VendorRequestsOut(void) {
     }
 }
 
-init_ardcom(void){
-    uart_open(&uart2, UARTTx, UARTRx, NULL, NULL, 9600., 'N', 1, 
-          0, NULL, 0, NULL, 0);
-}
-
 void init(void){
     init_pin();
     init_clock();
@@ -76,30 +75,78 @@ void init(void){
     init_ui();
     init_timer();
     init_oc();
-    InitUSB(); // initialize the USB registers and serial interface engine
+    InitUSB(); 
 
+    // oc_pwm(&oc1,CLK,CLK_TIMER,10e3,10000); //10kHZ at 50% Duty
+
+    timer_setPeriod(HB_TIMER, 0.3);
+    timer_start(HB_TIMER);
+
+    // max CLK freq is 15 kHz, four clock cycles 
+    timer_setPeriod(STARTUP_TIMER, 0.001); 
+    timer_start(STARTUP_TIMER);
+
+    timer_setPeriod(CLK_TIMER, 0.0022); 
+    timer_start(CLK_TIMER);
+
+    pin_digitalOut(CLK);
+    pin_digitalOut(ENABLE);
+    pin_digitalOut(RESET);
+    pin_digitalOut(CW);
+
+    pin_write(CLK,0);
+    pin_write(ENABLE,0);
+    pin_write(RESET,1);
+    pin_write(CW,0);
+    clk_out = 0;
 }
+
+void startup(void){
+    //make sure we follow the correct startup routine
+    // if (startup_counter < 4)
+    // {
+    //     pin_write(ENABLE,0);
+    //     pin_write(RESET,0);
+    // }
+    // else if (startup_counter >= 4 && startup_counter <= 8)
+    // {
+    //     pin_write(ENABLE,0);
+    //     pin_write(RESET,1);
+    // } else {
+        pin_write(ENABLE,1);
+        pin_write(RESET,1);
+    // }
+}
+
 
 int16_t main(void) {
     init();
-    val1 = 0;
-    val2 = 0;
-    led_on(&led2);
-    timer_setPeriod(HeartBeatTimer, 0.5);
-    timer_start(HeartBeatTimer);
-    printf("Good morning!\n");
-    
     while (USB_USWSTAT!=CONFIG_STATE) {     // while the peripheral is not configured...
         ServiceUSB();                       // ...service USB requests
     }
     while (1) {
-        val1 = uart_getc(&uart2);
-        val2 = uart_getc(&uart2);
         ServiceUSB();
-         if (timer_flag(HeartBeatTimer)) {
-            timer_lower(HeartBeatTimer);
+        pin_write(CLK,clk_out);
+         if (timer_flag(HB_TIMER)) {
+            timer_lower(HB_TIMER);
             led_toggle(&led1);
-        }      
+        }
+        if (timer_flag(STARTUP_TIMER)) {
+            timer_lower(STARTUP_TIMER);
+            ++startup_counter;
+        }
+        if (timer_flag(CLK_TIMER)) {
+            timer_lower(CLK_TIMER);
+            clk_out = !clk_out;
+            if (clk_out)
+            {
+                led_on(&led2);
+            }
+            else{
+                led_off(&led2);   
+            }
+        }
+        startup();
     }
 }
 
