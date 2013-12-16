a55ea35a2b07fe25b3beb45d67e14dd7cc7b2c56
Noam <noamr@outlook.com>
2013-12-15 22:32:47 -0500

    motors are responding well to pot control

--- stepperdriver/stepperdriver.c
+++ stepperdriver/stepperdriver.c
@@ -9,29 +9,32 @@
 #include "timer.h"
 #include <stdio.h>
 
-#define GOTO_POS    0   // Vendor request that receives 2 unenesigned integer values
-#define SET_0    1   // Vendor request that returns 2 unsigned integer values
-#define GET_POS 2 //Vender request that clears the rev counter
+#define HB_TIMER        &timer1
+#define CLK_TIMER       &timer2
+#define CONTROL_TIMER   &timer3
+#define MEASURE_TIMER   &timer4
+#define POT         &A[0]
+#define M1_CW       &D[0]
+#define M1_RESET    &D[1] 
+#define M1_ENABLE   &D[2]
+#define M1_CLK      &D[3]
+#define M2_CLK      &D[4]
+#define M2_ENABLE   &D[5]
+#define M2_RESET    &D[6] 
+#define M2_CW       &D[7]
+#define LIMIT1      &D[8]
+#define LIMIT2      &D[9]
 
-#define HB_TIMER &timer3
-#define CLK_TIMER &timer2
-#define STARTUP_TIMER &timer1
-#define POT &A[0]
-#define M1_CW &D[0]
-#define M1_RESET &D[1] 
-#define M1_ENABLE &D[2]
-#define M1_CLK &D[3]
-#define M2_CLK &D[4]
-#define M2_ENABLE &D[5]
-#define M2_RESET &D[6] 
-#define M2_CW &D[7]
-#define LIMIT1 &D[8]
-#define LIMIT2 &D[9]
-#define DEBOUNCE 200
+#define GOTO_POS    0   // Vendor request that receives 2 unsigned integer values
+#define SET_0       1   // Vendor request that returns  2 unsigned integer values
+#define GET_POS     2   // Vender request that clears the rev counter
 
+#define DEBOUNCE    200
+#define ARR_SIZE    10
+#define BAD_VAL     1000
 
-uint16_t m1_dpos, m2_dpos, m1_cpos, m2_cpos, val2, clk_out, m1_dir, m2_dir, m1_clk, m2_clk, limit1_counter,limit2_counter, limit1_prev, limit2_prev, pot_val;
-
+uint16_t val2, clk_out, m1_dpos, m2_dpos, m1_cpos, m2_cpos, m1_dir, m2_dir, m1_clk, m2_clk, limit1_counter,limit2_counter, limit1_prev, limit2_prev;
+uint16_t pot_val, prev_pot_val, new_pot_val;
 
 void VendorRequests(void) {
     WORD temp;
@@ -87,17 +90,17 @@ void init(void){
     init_oc();
     InitUSB(); 
 
-    // oc_pwm(&oc1,CLK,CLK_TIMER,10e3,10000); //10kHZ at 50% Duty
-
     timer_setPeriod(HB_TIMER, 0.3);
     timer_start(HB_TIMER);
 
-    // max CLK freq is 15 kHz, four clock cycles 
-    timer_setPeriod(STARTUP_TIMER, 0.001); 
-    timer_start(STARTUP_TIMER);
-
     timer_setPeriod(CLK_TIMER, 0.0022); 
     timer_start(CLK_TIMER);
+
+    timer_setPeriod(MEASURE_TIMER, 0.00001); 
+    timer_start(MEASURE_TIMER);
+
+    timer_setPeriod(CONTROL_TIMER, 0.001); 
+    timer_start(CONTROL_TIMER);
 
     pin_digitalOut(M1_ENABLE);
     pin_digitalOut(M1_CLK);
@@ -119,21 +122,25 @@ void init(void){
     pin_write(M2_RESET,0);
     pin_write(M2_CW,0);
 
-    m1_clk= 0;
-    m2_clk= 0;
+    m1_clk = 0;
+    m2_clk = 0;
     m1_cpos = 0;
     m1_dpos = 0;
 }
 
 void startup(void){
-        pin_write(M1_ENABLE,1);
-        pin_write(M1_RESET,1);
-        pin_write(M2_ENABLE,1);
-        pin_write(M2_RESET,1);
+    pin_write(M1_ENABLE,1);
+    pin_write(M1_RESET,1);
+    pin_write(M2_ENABLE,1);
+    pin_write(M2_RESET,1);
 }
 
 uint16_t within_interval(uint16_t actual, uint16_t desired, uint16_t interval){
-    if ((actual > desired - interval) && (actual < desired + interval))
+    /*
+    n - ( > n) -> 65536
+    65536-n + ()
+    */
+    if ( (actual - desired) <= interval || (desired - actual) <= interval )
     {
         return 1;
     } else {
@@ -142,9 +149,10 @@ uint16_t within_interval(uint16_t actual, uint16_t desired, uint16_t interval){
 }
 
 void move_m1(void){
-    // if (within_interval(m1_cpos, m1_dpos, 5))
-    if (m1_cpos != m1_dpos)
+    if (!within_interval(m1_cpos, m1_dpos, 5000))
     {
+        pin_write(M1_RESET, 1);
+        pin_write(M1_ENABLE, 1);
         if (m1_dpos > m1_cpos)
         {
             m1_dir = 1; //forward
@@ -153,24 +161,20 @@ void move_m1(void){
             m1_dir = 0; //backward
             --m1_cpos;
         }
-        m1_clk = !m1_clk;
-    }
-    pin_write(M1_CW, m1_dir);
-    pin_write(M1_CLK,m1_clk);
-    if (m1_clk)
-    {
-        led_on(&led2);
+        pin_write(M1_CW, m1_dir);
     } else {
-        led_off(&led2);
+        m1_dpos = m1_cpos;
+        pin_write(M1_ENABLE, 0);
+        pin_write(M1_RESET, 0);
+        pin_write(M1_CLK, 0);
     }
 }
 
 void move_m2(void){
-    /* We want to be within some interval of the desired position,
-    in order to avoid jitter. */
-    // if (within_interval(m2_cpos, m2_dpos, 5))
-    if (m2_cpos != m2_dpos)
+    if (!within_interval(m2_cpos, m2_dpos, 5000))
     {
+        pin_write(M2_RESET, 1);
+        pin_write(M2_ENABLE, 1);
         if (m2_dpos > m2_cpos)
         {
             m2_dir = 1; //forward
@@ -179,40 +183,46 @@ void move_m2(void){
             m2_dir = 0; //backward
             --m2_cpos;
         }
-        m2_clk = !m2_clk;
-    }
-    pin_write(M2_CW, m2_dir);
-    pin_write(M2_CLK,m2_clk);
-    if (m2_clk)
-    {
-        led_on(&led3);
+        pin_write(M2_CW, m2_dir);
     } else {
-        led_off(&led3);
+        m2_dpos = m2_cpos;
+        pin_write(M2_ENABLE, 0);
+        pin_write(M2_RESET, 0);
+        pin_write(M2_CLK, 0);
     }
 }
 
+void MEASURE_LOOP(void){
+    /*Should read the pot val and update the running average
+    (must filter)
+    */
+    
+    pot_val = pin_read(POT);
+    
+    if ( abs(pot_val - prev_pot_val) > BAD_VAL )
+    {
+        new_pot_val = prev_pot_val;
+    } else {
+        new_pot_val = .25*pot_val + .75*prev_pot_val;            
+        prev_pot_val = new_pot_val;
+    }
+    m1_dpos = new_pot_val;
+    m2_dpos = new_pot_val;
+}
 
-int16_t main(void) {
-    init();
-    printf("Hello World!\n");
-    m1_dpos = 65000;
-    m2_dpos = 65000;
-    m1_cpos = 30000;
-    m2_cpos = 30000;
-    // while (USB_USWSTAT!=CONFIG_STATE) {     // while the peripheral is not configured...
-    //     ServiceUSB();                       // ...service USB requests
-    // }
-    while (1) {
-        m1_dpos = pin_read(POT);
-        m2_dpos = pin_read(POT);
+void CONTROL_LOOP(void){
+    /*
+    Steps in controlling the steppers:
+    read the pot value running average
+    convert the pot value to a stepper setpoint (0 - 65536)
+    check if the stepper is within an acceptable value of the set point
+    */
+    move_m1();
+    move_m2();
+}
 
-        // ServiceUSB();
-         if (timer_flag(HB_TIMER)) {
-            timer_lower(HB_TIMER);
-            led_toggle(&led1);
-        }
-
-        if (pin_read(LIMIT1))
+void STOP_LOOP(void){
+     if (pin_read(LIMIT1))
         {
             if (limit1_prev)
             {
@@ -247,13 +257,55 @@ int16_t main(void) {
             m2_dpos = m2_cpos;
             limit2_counter = 0;
         }
+}
+
+void SIGNAL_LOOP(void){
+    clk_out = !clk_out;
+    pin_write(M1_CLK,clk_out);
+    pin_write(M2_CLK,clk_out);
+}
+
+int16_t main(void) {
+
+    init();
+    startup();
+    printf("Hello World!\n");
+
+    m1_dpos = 0;
+    m2_dpos = 0;
+    m1_cpos = 0;
+    m2_cpos = 0;
+
+    // while (USB_USWSTAT!=CONFIG_STATE) {     // while the peripheral is not configured...
+    //     ServiceUSB();                       // ...service USB requests
+    // }
+
+    while (1) {
+
+        // ServiceUSB();
+         // if (timer_flag(HB_TIMER)) {
+         //    timer_lower(HB_TIMER);
+         //    led_toggle(&led1);
+         //    // printf("pot = %u, new_pot = %u\n", pot_val, new_pot_val);
+         //    // printf("pot = %i, range1 = %i, range2 = %i, prev = %i\n", pot_val, pot_val+BAD_VAL, pot_val-BAD_VAL, prev_pot_val);
+         //    printf("pot = %u, new = %u, prev = %u, desired = %u, current = %u, delta = %u\n", pot_val, new_pot_val, prev_pot_val, m1_dpos, m1_cpos, m1_dpos-m1_cpos);
+        // }
+
+        if (timer_flag(MEASURE_TIMER)) {
+            timer_lower(MEASURE_TIMER);
+            MEASURE_LOOP();
+        }
+
+        if (timer_flag(CONTROL_TIMER)) {
+            timer_lower(CONTROL_TIMER);
+            CONTROL_LOOP();
+        }
+
+       
         if (timer_flag(CLK_TIMER)) {
             timer_lower(CLK_TIMER);
-            move_m1();
-            move_m2();
-            printf("%u\n", m1_dpos);
+            SIGNAL_LOOP();
         }
-        startup();
     }
 }
 
