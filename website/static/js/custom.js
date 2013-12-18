ddfeba04a2c5c86661fed732bcae520b392cf51c
Noam <noamr@outlook.com>
2013-12-18 18:25:35 -0500

    added lazy image loading, team section, and lorem ipsum text

--- website/static/js/custom.js
+++ website/static/js/custom.js
@@ -2,6 +2,7 @@
     "use strict"
 
     $(document).ready(function() {
+    	$("img.lazy").lazyload({effect:"fadeIn"});
         var sectionid = "overview";
         $(".navbar-nav li a[href='#" + sectionid + "']").addClass("active");
         autoHighlightNavBar();
@@ -12,9 +13,6 @@
         var hrefs = []; // create the empty hrefs
         for (var i = 0; i < navItems.length; i++) {
             hrefs.push($(navItems[i]).attr("href"));
-            // var aChild = navItems[i];
-            // var ahref = $(aChild).attr('href');
-            // hrefs.push(ahref);
         }
         $(window).scroll(function() {
             var windowPos = $(window).scrollTop(); // get the offset of the window from the top of page
@@ -26,7 +24,7 @@
                 var sectionid = hrefs[i];
                 var divPos = $(sectionid).offset().top; // get the offset of the div from the top of page
                 var divHeight = $(sectionid).height(); // get the height of the div in question
-                if (windowPos >= divPos - offset && windowPos < (divPos + divHeight + offset)) {
+                if (windowPos >= divPos - offset && windowPos < (divPos + divHeight - offset)) {
                     $("a[href='" + sectionid + "']").addClass("nav-active");
                 } else {
                     $("a[href='" + sectionid + "']").removeClass("nav-active");
